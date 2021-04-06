f8eeab2dc4af46761e2234208a7dab487bbc225f
Ilkka Miettinen <ivmiettinen@gmail.com>
2021-04-06 09:26:47 +0300

    new challenges

---
+++ medium/burg.js
@@ -0,0 +1,10 @@
+function addName(obj, name, value) {
+	
+}
+
+
+
+
+addName({ piano: 500, stereo: 300 }, "Caligula", 440) 
+
+//➞ { piano: 500, stereo: 300, Caligula: 440 }
---
+++ medium/nestedArray.js
@@ -0,0 +1,20 @@
+
+//esimerkki:
+
+function canNest(arr1, arr2) {
+	return Math.min(...arr1) > Math.min(...arr2) && Math.max(...arr1) < Math.max(...arr2);
+}
+
+
+//Oma:
+
+function canNest(arr1, arr2) {
+  if (
+    Math.min(...arr1) > Math.min(...arr2) &&
+    Math.max(...arr1) < Math.max(...arr2)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
