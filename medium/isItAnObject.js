5d4406a805118b7cac805cad359d1f01b8a56cb9
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2021-05-12 08:33:06 +0300

    more challenges

--- medium/isItAnObject.js
+++ medium/isItAnObject.js
@@ -4,21 +4,4 @@
 // Notes
 // Inputs may be null, primitive wrapper types, dates.
 
-function isObject(value) {
-    console.log('value', value)
-    // return typeof yourVariable === 'object' && yourVariable !== null
-
-    return value instanceof Object
-}
-
 const isObject = (value) => value instanceof Object
-
-isObject(function add(x, y) {
-    return x + y
-})
-
-function C() {}
-
-let o = new C()
-
-o instanceof C
